945d80ab8cccf0b4af16bb8c29b4892b95b14641
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2017-11-21 10:56:18 +0900

    Add screen shot

--- src/main/scala/me/shoma/puppeteer4s/Line.scala
+++ src/main/scala/me/shoma/puppeteer4s/Line.scala
@@ -7,39 +7,34 @@ trait Line {
 }
 
 case class Browser() extends Line {
-  def newPage(): Page = new Page(this)
-  override def lines: Seq[Line] = Seq(this)
+  def newPage(): Page = new Page(this, lines)
+
+  override def lines = Seq(this)
   override def compile: String = "const browser = await puppeteer.launch();"
 }
 
 class ReturnSyntax(line: Line) extends Line {
-  override def lines = line.lines.init :+ this
-  override def compile = "return " + line.compile
+  override def lines: Seq[Line] = line.lines.init :+ this
+  override def compile: String = "return " + line.compile
 }
 
-class Page(browser: Browser) extends Line {
+class Page(browser: Browser, list: Seq[Line]) extends Line {
 
-  lazy val self = new Page(this.browser)
-
-  def goto(url: String): Page = {
-    new Page(this.browser) {
-      override def lines: Seq[Line] = self.lines :+ this
-      override def compile: String = s"await page.goto('$url');"
-    }
+  def goto(url: String): Page = new Page(browser, list :+ this) {
+    override def compile: String = s"await page.goto('$url');"
   }
 
-  def evaluate(): Page = {
-    new Page(this.browser) {
-      override def lines: Seq[Line] = self.lines :+ this
-      override def compile: String = "await page.evaluate(() => document.body.innerHTML);"
-    }
+  def evaluate(): Page = new Page(browser, list :+ this) {
+    override def compile: String = "await page.evaluate(() => document.body.innerHTML);"
   }
 
-  def toReturn: ReturnSyntax = {
-    new ReturnSyntax(this)
+  def screenShot(path: String, fullPage: Boolean): Page = new Page(browser, list :+ this) {
+    override def compile: String = s"await page.screenshot({path: '$path', fullPage: $fullPage});"
   }
 
-  override def lines: Seq[Line] = browser.lines :+ this
+  def toReturn: ReturnSyntax = new ReturnSyntax(this)
+
+  override def lines: Seq[Line] = list :+ this
   override def compile = "const page = await browser.newPage();"
 }
 
@@ -58,6 +53,7 @@ object App extends App {
   val program = Browser()             // create browser
     .newPage()                        // create page instance
     .goto("https://www.google.com")   // go to google.com
+    .screenShot("test.png", true)     // take a screen shot
     .evaluate()                       // get html source
     .toReturn                         // return html source
 
@@ -70,6 +66,8 @@ object App extends App {
   async function run() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
+    await page.goto('https://www.google.com');
+    await page.screenshot({path: 'test.png', fullPage: true});
     return await page.evaluate(() => document.body.innerHTML);
   }
 
